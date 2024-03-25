7135ffbef587d52436805a3c13d041babe80dc8b
ArthurCost4 <arthurlinhares69@gmail.com>
2024-03-25 20:00:20 +0000

    Script IaC para provisionamento de um servidor apache terminado

--- script.sh
+++ script.sh
@@ -4,25 +4,33 @@ echo 'Atualizando o servidor'
 
 #apt-get update && apt-get upgrade
 
-# Verifica se o pacote do Apache está instalado
+# Verifica se o pacote do Apache está instalado caso não ja o faz
 if dpkg -l | grep -q apache2; then
     echo "O Apache ja está instalado."
+    apache2 -v
 else
     echo "instalando o Apache2"
-    #apt-get install apache2 -y
+    apt-get install apache2 -y
     echo 'Eis a versão do apache'
-    # apache2 -v
+    apache2 -v
 fi
 
-# Verifica se ja tem o unzip instalado
+# Verifica se ja tem o unzip instalado caso não ja o faz
 if dpkg -l | grep -q unzip; then
-	echo 'ja tem'
+	echo 'ja tem o unzip instalado'
+
 else
-	echo 'não tem'
-	#apt-get install unzip -y
+	echo 'não tem o unzip instalado'
+	apt-get install unzip -y
 fi
 
-echo 'Qual o link do repositorio ou site que deseja instalado?'
+echo 'Qual o link do repositorio ou site que deseja instalar?'
 read -r link_repo
 
-echo "$link_repo"
+cd /tmp/ && git clone "$link_repo" main
+
+#unzip main.zip
+
+rm -r /var/www/html/*
+cd /tmp/ && cp -rvf main/* /var/www/html/
+systemctl status apache2.service
